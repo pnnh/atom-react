7b7ddc44ce0a998e614e07a0400e96568645af6b
larry <xspanni@gmail.com>
2024-09-05 19:14:14 +0800

    增加jsx和html模板辅助方法

--- src/index.common.tsx
+++ src/index.common.tsx
@@ -3,6 +3,8 @@ export * from './common/error'
 export * from './common/link'
 export * from './common/loading'
 export * from './common/pagination'
+export * from './common/component'
+export * from './common/jsxFactory'
 
 export function sayHelloCommon(word: string) {
     return "Hello from SteleCommon!" + word
